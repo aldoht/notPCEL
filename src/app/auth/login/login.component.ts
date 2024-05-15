35285cf04df11aa1996bb292e79455d9b4789c15
Damian G <gilbertodamian14@gmail.com>
2024-05-14 23:47:28 -0600

    feat: add a service for notifications, and use it on the login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../auth.service";
+import {NotificationsService} from "../../notifications.service";
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,7 @@ import {AuthService} from "../../auth.service";
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private notService: NotificationsService) {
   }
 
   public usuario: string = "";
@@ -18,6 +19,33 @@ export class LoginComponent implements OnInit {
   }
 
   generar() {
-    this.auth.login(this.usuario, this.passwd)
+    this.auth.login(this.usuario, this.passwd).then(value => {
+      this.notService.createNotification("Login exitoso", "#aabbcc", 1500);
+
+    }).catch(reason => {
+      let message;
+      console.log(reason)
+
+
+      switch (reason['code']) {
+        case "auth/invalid-email":
+          message = "Usuario no encontrado.";
+          break;
+        case "auth/user-disabled":
+          message = "Tu usuario ha sido deshabilitado."
+          break;
+        case "auth/user-not-found":
+          message = "Usuario no encontrado.";
+          break;
+        case "auth/wrong-password":
+          message = "Contraseña incorrecta";
+          break;
+        default:
+          message = "Error desconocido";
+          break;
+      }
+
+      this.notService.createNotification(message, "danger", 1500);
+    })
   }
 }
