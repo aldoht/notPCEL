731573a9c786890e65b9f1188207351ba72fed60
aldoht <aldofernandoht@gmail.com>
2024-05-22 12:33:48 -0600

    Se agregó la vista de favoritos

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesPage implements OnInit {
 
-  constructor() { }
+  /** Hay que implementar el servicio para favoritos */
+  favoritosArray: any[] = [
+    {
+      id: 1,
+      quantity: "-1",
+      name: "NombreProducto",
+      description: "N/A",
+      unitPrice: "-1",
+      photoURL: "https://m.media-amazon.com/images/I/61-7uGhZfQL._AC_UF894,1000_QL80_.jpg"
+    }
+  ]
 
-  ngOnInit() {
+  constructor(private router: Router) { }
+
+  ngOnInit() {  }
+
+  navigateToPage(productId: string) {
+    this.router.navigate(['/home/products/', productId]);
   }
-
 }
